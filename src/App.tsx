4fff6ee68688e136b81975bc9c24f614918133e3
SenkeSem <sen9morf@gmail.com>
2024-04-11 14:59:22 +0300

    Настраивает проект

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <h1>Тестовое задание для LogicLike</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import styles from './Sidebar.module.scss';
+
+const Sidebar = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h4>Меню выбора игр</h4>
+    </div>
+  );
+};
+
+export default Sidebar;
