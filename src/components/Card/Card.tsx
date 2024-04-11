e39fee8a5661dc14e7ffdad35b0ef419f1e665f9
SenkeSem <sen9morf@gmail.com>
2024-04-11 16:38:43 +0300

    Добавляет меню и получает json

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+import styles from './Card.module.scss';
+
+const Card = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h5>Карточка товара</h5>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/CourseList/CourseList.tsx
+++ src/components/CourseList/CourseList.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
+import Card from '../Card/Card';
 import styles from './CourseList.module.scss';
 
+import axios from 'axios';
+
 const CourseList = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    async function getCourses() {
+      try {
+        const { data } = await axios.get('https://logiclike.com/docs/courses.json');
+
+        setCourses(data);
+        console.log(data);
+      } catch (err) {
+        alert('При получение курсов произошла ошибка!');
+      }
+    }
+    getCourses();
+  }, []);
+
   return (
     <div className={styles.wrapper}>
-      <h4>Тут будут располагаться карточки курсов</h4>
+      {courses.map((item) => (
+        <>
+          <Card />
+        </>
+      ))}
     </div>
   );
 };
